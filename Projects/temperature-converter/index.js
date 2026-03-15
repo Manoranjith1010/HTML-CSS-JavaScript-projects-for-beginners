bfb05c158246f09ecbf18cc44df0b10c18f1a1be
Manoranjith <164288436+Manoranjith1010@users.noreply.github.com>
2026-03-15 15:26:20 +0000

    temperature-converter

--- Projects/temperature-converter/index.js
+++ Projects/temperature-converter/index.js
@@ -1,49 +1,24 @@
-<!DOCTYPE html>
-<html lang="en">
-  <head>
-    <meta charset="UTF-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>Temperature Converter</title>
-    <link rel="stylesheet" href="style.css" />
-  </head>
-  <body>
-    <div class="container">
-      <h1 class="heading">Temperature Converter</h1>
-      <div class="temp-container">
-        <label for="celsius">Celsius:</label>
-        <input
-          onchange="computeTemp(event)"
-          type="number"
-          name="celsius"
-          id="celsius"
-          placeholder="Enter Temperature"
-          class="input"
-        />
-      </div>
-      <div class="temp-container">
-        <label for="fahrenheit">Fahrenheit:</label>
-        <input
-          onchange="computeTemp(event)"
-          type="number"
-          name="fahrenheit"
-          id="fahrenheit"
-          placeholder="Enter Temperature"
-          class="input"
-        />
-      </div>
-      <div class="temp-container">
-        <label for="kelvin">Kelvin:</label>
-        <input
-          onchange="computeTemp(event)"
-          type="number"
-          name="kelvin"
-          id="kelvin"
-          placeholder="Enter Temperature"
-          class="input"
-        />
-      </div>
-    </div>
-    <script src="index.js"></script>
-  </body>
-</html>
+const celsiusEl = document.getElementById("celsius");
+const fahrenheitEl = document.getElementById("fahrenheit");
+const kelvinEl = document.getElementById("kelvin");
+
+function computeTemp(event) {
+  const currentValue = +event.target.value;
+
+  switch (event.target.name) {
+    case "celsius":
+      kelvinEl.value = (currentValue + 273.32).toFixed(2);
+      fahrenheitEl.value = (currentValue * 1.8 + 32).toFixed(2);
+      break;
+    case "fahrenheit":
+      celsiusEl.value = ((currentValue - 32) / 1.8).toFixed(2);
+      kelvinEl.value = ((currentValue - 32) / 1.8 + 273.32).toFixed(2);
+      break;
+    case "kelvin":
+      celsiusEl.value = (currentValue - 273.32).toFixed(2);
+      fahrenheitEl.value = ((currentValue - 273.32) * 1.8 + 32).toFixed(2);
+      break;
+    default:
+      break;
+  }
+}
